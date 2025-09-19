3e7d948b862df5f0b3eed81b117b08a0e766932a
Bao Nguyen <bnn@bu.edu>
2025-09-19 10:08:05 -0400

    Add button linking to BU

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -77,20 +77,20 @@ export default function Home() {
           Examples
         </a>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://bu.edu"
           target="_blank"
           rel="noopener noreferrer"
-        >
+          >
           <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
+          aria-hidden
+          src="/globe.svg"
+          alt="Globe icon"
+          width={16}
+          height={16}
           />
-          Go to nextjs.org →
-        </a>
+          Go to bu.edu →
+          </a>
+
       </footer>
     </div>
   );
